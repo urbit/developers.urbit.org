25483f9b9a804c49241cf0b9091764a320fcefcb
James Acklin <james.acklin@gmail.com>
2021-10-04 10:05:10 -0400

    typo

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ module.exports = {
   purge: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
-    "./content/**/*.{md,mdx",
+    "./content/**/*.{md,mdx}",
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
