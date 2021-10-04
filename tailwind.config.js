b2464147e48a64e327d37a53411e21338cdbcec4
James Acklin <james.acklin@gmail.com>
2021-10-04 10:04:54 -0400

    don't purge tailwind from mdx content

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,9 @@
 module.exports = {
-  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  purge: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./content/**/*.{md,mdx",
+  ],
   darkMode: false, // or 'media' or 'class'
   theme: {
     colors: {
