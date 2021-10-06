09b52ce8aedf4fdfbeeed37b02cffb24f492821e
James Acklin <james.acklin@gmail.com>
2021-10-06 12:59:10 -0400

    nicer directory cards

--- pages/community.js
+++ pages/community.js
@@ -1,6 +1,5 @@
 import Head from "next/head";
 import matter from "gray-matter";
-
 import html from "remark-html";
 import { Remark } from "react-remark";
 import Layout from "../components/Layout";
@@ -24,18 +23,24 @@ export default function Post() {
           </Section>
           <Section>
             <div className="prose lg:prose-lg">
-              <Remark>{content}</Remark>
+              <Remark remarkPlugins={[html]}>{content}</Remark>
             </div>
             <div className="prose lg:prose-lg">
-              <div className="py-8 grid gap-8 lg:grid-cols-2">
+              <div className="py-8 grid gap-8 xl:grid-cols-2">
                 {data.directory.map((person, i) => (
                   <div className="p-8 rounded-xl bg-wall-100" key={person.patp}>
-                    <Sigil patp={person.patp} />
-                    <div>
-                      <code>{person.patp}</code>
+                    <div className="flex">
+                      <div className="mr-4">
+                        <Sigil patp={person.patp} />
+                      </div>
+                      <div>
+                        <div className="font-mono font-semibold">
+                          {person.patp}
+                        </div>
+                        <div className="text-sm">{person.job}</div>
+                      </div>
                     </div>
-                    <div>{person.job}</div>
-                    <div>{person.roles}</div>
+                    <div className="mt-4">{person.roles}</div>
                   </div>
                 ))}
                 <div>
