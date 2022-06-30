c2798fb415616885830d27ed8fa8df6c7d9222f4
Matilde Park <m@park.computer>
2022-06-29 17:25:47 -0700

    guides, reference: fix highlight on drawer nav

--- pages/reference/[[...slug]].js
+++ pages/reference/[[...slug]].js
@@ -125,11 +125,12 @@ const pageTree = (thisLink, tree, level = 0) => {
   });
 
   const headingItemClasses = classnames({
-    "pl-0 text-wall-600 text-base font-semibold hover:text-green-400 leading-relaxed":
+    "pl-0 text-base font-semibold hover:text-green-400 leading-relaxed":
       level === 0,
-    "pl-4 text-wall-600 text-base font-semibold hover:text-green-400":
-      level === 1,
-    "pl-8 text-wall-600 text-base hover:text-green-400": level === 2,
+    "pl-4 text-base font-semibold hover:text-green-400": level === 1,
+    "pl-8 text-base hover:text-green-400": level === 2,
+    "dot text-green-400": isThisPage,
+    "text-wall-600": !isThisPage,
   });
 
   const pageItemClasses = classnames({
@@ -173,7 +174,9 @@ const pageTree = (thisLink, tree, level = 0) => {
     <>
       <Link href={thisLink} passHref>
         <a onClick={() => toggleTree(!isOpen)}>
-          <p className={`${headingItemClasses} cursor-pointer`}>{tree.title}</p>
+          <p className={`${headingItemClasses} relative cursor-pointer`}>
+            {tree.title}
+          </p>
         </a>
       </Link>
       <div className={activeClasses}>
