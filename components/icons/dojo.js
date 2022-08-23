ed6539fdb29409c681189f8fa21ac15a6d837983
Matilde Park <m@park.computer>
2022-08-22 21:27:30 -0700

    icons: make dojo icon responsive

--- components/icons/dojo.js
+++ components/icons/dojo.js
@@ -7,10 +7,10 @@ export default function Dojo() {
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
     >
-<path d="M61 32H34V20H61V32ZM46 47H34V35H46V47ZM19 20H31V47H19V20ZM19 50H46V62H19V50ZM61 62H49V35H61V62Z" stroke="black"/>
-
+      <path
+        d="M61 32H34V20H61V32ZM46 47H34V35H46V47ZM19 20H31V47H19V20ZM19 50H46V62H19V50ZM61 62H49V35H61V62Z"
+        className="stroke-black"
+      />
     </svg>
   );
 }
-
-
