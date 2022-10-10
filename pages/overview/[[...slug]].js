64f1cbf3b247232c6bb68c9d75dbd6dadbbd211a
Matilde Park <m@park.computer>
2022-10-10 16:58:36 -0700

    pages: show last modified on docs content

--- pages/overview/[[...slug]].js
+++ pages/overview/[[...slug]].js
@@ -79,14 +79,17 @@ export default function Overview({
               />
             )}
           </div>
-          <a
-            className="font-semibold rounded-xl block p-2 text-wall-400 hover:text-green-400 mt-16"
-            target="_blank"
-            href={`https://github.com/urbit/developers.urbit.org/blob/master/content/overview/${params.slug?.join("/") || "_index"
-              }.md`}
-          >
-            Edit this page on GitHub
-          </a>
+          <div className="flex justify-between items-center mt-16">
+            <a
+              className="font-semibold rounded-xl block p-2 text-wall-400 hover:text-green-400"
+              target="_blank"
+              href={`https://github.com/urbit/developers.urbit.org/blob/master/content/guides/${params.slug?.join("/") || "_index"
+                }.md`}
+            >
+              Edit this page on GitHub
+            </a>
+            <p className="font-semibold block p-2 text-wall-400">Last modified {data.lastModified}</p>
+          </div>
         </ContentArea>
       </div>
     </>
@@ -345,7 +348,7 @@ export async function getStaticProps({ params }) {
   let posts = overviewTree;
 
   const { data, content } = getPage(
-    join(process.cwd(), "content/overview", params.slug?.join("/") || "/")
+    join(process.cwd(), "content/overview", params.slug?.join("/") || "/"), true
   );
 
   const previousPost =
