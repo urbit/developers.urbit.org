e7147084a4cb485d64cdf3f6a2b804e786017b3e
Matilde Park <m@park.computer>
2022-10-19 12:28:56 -0700

    api: add urbit.org staging

--- pages/api/urbit-org-search.js
+++ pages/api/urbit-org-search.js
@@ -1,6 +1,6 @@
 export default async (req, res) => {
   const results = await fetch(
-    `https://urbit.org/api/search?q=${req.query.q}`
+    `https://urbit-5cl3gjdl1-urbit.vercel.app/api/search?q=${req.query.q}`
   ).then((res) => res.json());
   res.statusCode = 200;
   res.setHeader("Content-Type", "application/json");
