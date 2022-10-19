da0be3ffcb05208d6a76c168be613c26544239df
Matilde Park <m@park.computer>
2022-10-18 21:33:32 -0700

    api/ops-search: stub dev deploy for ops search

--- pages/api/ops-search.js
+++ pages/api/ops-search.js
@@ -1,6 +1,6 @@
 export default async (req, res) => {
   const results = await fetch(
-    `https://operators.urbit.org/api/search?q=${req.query.q}`
+    `https://operators-urbit-kpkx6sfyq-urbit.vercel.app/api/search?q=${req.query.q}`
   ).then((res) => res.json());
   res.statusCode = 200;
   res.setHeader("Content-Type", "application/json");
