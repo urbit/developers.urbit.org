5be3098a0df8a7a1b8f8c6532af20aed233e4f89
Matilde Park <m@park.computer>
2022-11-07 16:25:07 -0800

    icons: add course icons

---
+++ components/icons/TallCard/hoonSchoolLive.js
@@ -0,0 +1,23 @@
+export default function HoonSchoolLive({ className = "" }) {
+    return <svg width="408" height="230" viewBox="0 0 408 230" className={className} fill="none" xmlns="http://www.w3.org/2000/svg">
+        <rect width="408" height="230" rx="10" fill="#5B7769" />
+        <path d="M127.586 84.2174C127.569 83.2063 127.734 82.3929 128.082 81.7773C128.429 81.1617 128.88 80.7135 129.434 80.4327C129.994 80.1519 130.584 80.0111 131.205 80.0104C131.825 80.0097 132.391 80.1461 132.901 80.4196C133.418 80.687 134.008 81.1097 134.67 81.6876C135.12 82.0769 135.478 82.3506 135.746 82.5086C136.019 82.6667 136.317 82.7455 136.64 82.7452C137.163 82.7446 137.577 82.5553 137.881 82.1773C138.186 81.7932 138.333 81.2387 138.321 80.5139L140.292 80.5117C140.309 81.5228 140.144 82.3361 139.797 82.9517C139.455 83.5673 139.005 84.0155 138.445 84.2963C137.885 84.5772 137.294 84.7179 136.674 84.7186C136.053 84.7194 135.488 84.586 134.977 84.3186C134.467 84.0451 133.877 83.6194 133.209 83.0415C132.765 82.6521 132.406 82.3785 132.133 82.2204C131.859 82.0623 131.561 81.9835 131.239 81.9839C130.746 81.9844 130.338 82.1585 130.016 82.5061C129.699 82.8475 129.546 83.4172 129.557 84.2151L127.586 84.2174Z" className="fill-white" />
+        <path fill-rule="evenodd" clip-rule="evenodd" d="M212.189 84.8516L152.77 84.9195L152.771 83.3115L212.191 83.2435L212.189 84.8516Z" className="fill-white" />
+        <path fill-rule="evenodd" clip-rule="evenodd" d="M212.128 137.917L127.014 138.015L127.016 136.407L212.13 136.309L212.128 137.917Z" className="fill-white" />
+        <path fill-rule="evenodd" clip-rule="evenodd" d="M212.152 117.013L152.733 117.081L152.734 115.473L212.154 115.405L212.152 117.013Z" className="fill-white" />
+        <path fill-rule="evenodd" clip-rule="evenodd" d="M188.088 96.1355L127.063 96.2053L127.064 94.5973L188.09 94.5275L188.088 96.1355Z" className="fill-white" />
+        <path fill-rule="evenodd" clip-rule="evenodd" d="M212.177 96.108L191.3 96.1319L191.302 94.5238L212.179 94.4999L212.177 96.108Z" className="fill-white" />
+        <path fill-rule="evenodd" clip-rule="evenodd" d="M188.051 128.297L127.026 128.366L127.027 126.758L188.053 126.689L188.051 128.297Z" className="fill-white" />
+        <path fill-rule="evenodd" clip-rule="evenodd" d="M165.592 105.81L127.05 105.854L127.052 104.246L165.594 104.202L165.592 105.81Z" className="fill-white" />
+        <path fill-rule="evenodd" clip-rule="evenodd" d="M165.542 149.227L127 149.271L127.002 147.663L165.544 147.619L165.542 149.227Z" className="fill-white" />
+        <path fill-rule="evenodd" clip-rule="evenodd" d="M149.521 117.084L127.038 117.11L127.04 115.502L149.523 115.476L149.521 117.084Z" className="fill-white" />
+        <path fill-rule="evenodd" clip-rule="evenodd" d="M212.139 128.269L191.262 128.293L191.264 126.685L212.141 126.661L212.139 128.269Z" className="fill-white" />
+        <path fill-rule="evenodd" clip-rule="evenodd" d="M212.164 105.756L170.41 105.804L170.412 104.196L212.166 104.148L212.164 105.756Z" className="fill-white" />
+        <path fill-rule="evenodd" clip-rule="evenodd" d="M212.116 149.174L170.361 149.221L170.363 147.613L212.117 147.566L212.116 149.174Z" className="fill-white" />
+        <rect x="77" y="49" width="185" height="132" rx="9" className="stroke-white stroke-2" />
+        <rect x="272" y="48" width="61" height="39" rx="13" className="stroke-white stroke-2" />
+        <rect x="272" y="95" width="61" height="39" rx="13" className="stroke-white stroke-2" />
+        <rect x="272" y="142" width="61" height="39" rx="13" className="stroke-white stroke-2" />
+    </svg>
+
+}
