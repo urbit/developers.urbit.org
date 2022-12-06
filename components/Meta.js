0c5d6ba33edb9602d6f2c2cf1b780a06e5841508
Gordon <gordon@hey.com>
2022-12-06 15:30:07 -0800

    fixing opengraph images

--- components/Meta.js
+++ components/Meta.js
@@ -4,10 +4,30 @@ export default function Meta(post, disableImage) {
   const description =
     post?.description || "Urbit provides foundational primitives at the operating system layer, serving as a better platform for building networked, decentralized applications.";
   const image =
-    post?.extra?.image || "https://storage.googleapis.com/media.urbit.org/site/opengraph/developers.png";
+    post?.extra?.image || "https://storage.googleapis.com/media.urbit.org/site/opengraph/sig-white.png";
   return (
-      
     <>
+
+      <link rel="icon" type="image/png" href="/images/favicon.ico" />
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/images/apple-touch-icon.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/images/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/images/favicon-16x16.png"
+      />
+      <link rel="manifest" href="/images/site.webmanifest" />
+      
       <meta
         name="twitter:card"
         content="summary_large_image"
@@ -26,6 +46,7 @@ export default function Meta(post, disableImage) {
       {!disableImage && (
         <meta name="twitter:image" content={image} key="image" />
       )}
+
 
     </>
   );
--- pages/_app.js
+++ pages/_app.js
@@ -64,27 +64,6 @@ function MyApp({ Component, pageProps }) {
 
   return (
     <>
-      <Head>
-        <link rel="icon" type="image/png" href="/images/favicon.ico" />
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/images/apple-touch-icon.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/images/favicon-32x32.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/images/favicon-16x16.png"
-        />
-        <link rel="manifest" href="/images/site.webmanifest" />
-      </Head>
 
       <GlobalHotKeys keyMap={keyMap} handlers={handlers} />
       <Search
