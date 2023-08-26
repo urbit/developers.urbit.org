60515cb450f7622202f3b84eaca69ef55b5603bc
jack-z-wang <81378423+jack-z-wang@users.noreply.github.com>
2023-08-25 20:45:51 -0400

    added core category

--- pages/hackathon.js
+++ pages/hackathon.js
@@ -40,7 +40,7 @@ export default function Hackathon({ search, featuredGroups }) {
   const post = {
     title: "Urbit Assembly Hackathon",
     description:
-      "WIll you be the one who builds Urbit’s killer app? This is your chance.",
+      "Unleash your creativity and join the quest to build something great.",
     image:
       "https://storage.googleapis.com/media.urbit.org/developers/hackathon/hackathon_logo.svg",
   };
@@ -68,7 +68,7 @@ export default function Hackathon({ search, featuredGroups }) {
 
           <div className="pt-8 md:p-16">
             <p className="type-ui text-center">
-            Unleash your creativity and join the quest to build Urbit's killer app. This year’s Assembly Hackathon has the most substantial prize pool in Urbit history. We invite you to rise to the challenge, showcase your skills, and claim victory.
+            Unleash your creativity and join the quest to build something great. This year’s Assembly Hackathon has the most substantial prize pool in Urbit history. We invite you to rise to the challenge, showcase your skills, and claim victory.
               </p>
             <p className="type-ui pt-8 text-center">
             In the first week you’ll enter as an individual and form a team, or register an existing team. Each team will get a mentor who guides you through the Hackathon, helps you solve issues, and keeps you on track. As you progress, they will provide regular meetings to ensure your product reaches its full potential. You'll have access to a network of UF team members, valuable resources, and experts from within the Urbit ecosystem. With a dedicated mentor by your side, workshops from our best developers, and a powerhouse team, all that's left for you to provide is the talent and determination.
@@ -90,7 +90,7 @@ export default function Hackathon({ search, featuredGroups }) {
           {/* Overall */}
           <div className="flex flex-col md:flex-row items-center space-y-8 md:space-y-0 md:space-x-8">
             <div className="flex flex-col space-y-4 max-w-prose">
-              <h3>Best overall – 3 Stars + 4 unreleased Native Planet devices</h3>
+              <h3>Best overall – 3 Stars + unreleased Native Planet devices</h3>
               <p>
               On Demo Day the committee of judges will decide which product has the highest score overall. The criteria they will take into account are: innovation, UI design, user value, usage of Urbit, and completeness.
               </p>
@@ -105,10 +105,19 @@ export default function Hackathon({ search, featuredGroups }) {
               </p>
             </div>
           </div>
+          {/* Core */}
+          <div className="flex flex-col-reverse md:flex-row items-center md:space-x-8">
+            <div className="flex flex-col space-y-4 max-w-prose">
+              <h3>Core – 2 Stars + unreleased Native Planet devices </h3>
+              <p>
+              Hardcore hacker? This is for you. This prize will be awarded to the best project that improves Urbit's developer experience or technical capabilities. Possible topics include developer tooling, system utilities, networking, security, performance, and more.
+              </p>
+            </div>
+          </div>
           {/* UI */}
           <div className="flex flex-col md:flex-row items-center space-y-8 md:space-y-0 md:space-x-8">
             <div className="flex flex-col space-y-4 max-w-prose">
-              <h3>Best UI – 2 Stars + 3 unreleased Native Planet devices</h3>
+              <h3>Best UI – 2 Stars + unreleased Native Planet devices</h3>
               <p>
               The Best UI winner will be chosen based on visual appeal, user experience, clarity, responsiveness, and interactivity.
               </p>
@@ -117,7 +126,7 @@ export default function Hackathon({ search, featuredGroups }) {
           {/* Composability */}
           <div className="flex flex-col-reverse md:flex-row items-center md:space-x-8">
             <div className="flex flex-col space-y-4 max-w-prose">
-              <h3>Best Composability – 2 Stars + 3 unreleased Native Planet devices</h3>
+              <h3>Best Composability – 2 Stars + unreleased Native Planet devices</h3>
               <p>
               A huge advantage of an Urbit personal server is app composability. Apps can be built to seamlessly interact with each other while keeping data local and private. This prize will be awarded to the product that makes best use of this by showing an innovative and useful integration with other Urbit services.
               </p>
@@ -126,7 +135,7 @@ export default function Hackathon({ search, featuredGroups }) {
           {/* Hooner */}
           <div className="flex flex-col-reverse md:flex-row items-center md:space-x-8">
             <div className="flex flex-col space-y-4 max-w-prose">
-              <h3>Rising Stars – 4 unreleased Native Planet devices </h3>
+              <h3>Rising Stars – Unreleased Native Planet devices </h3>
               <p>
               This prize will be given to the best product built by a team composed of at least 50% graduates of Hoon School since Assembly 2022.
               </p>
